2582fb2704fd57031c6ba798d17eb981035d2625
naaf <serranashraf@gmail.com>
2017-02-20 22:49:51 +0100

    add reflexion

---
+++ src/exception/DarException.java
@@ -0,0 +1,26 @@
+package exception;
+
+@SuppressWarnings("serial")
+public class DarException extends Exception{
+
+	public DarException(String message) {
+		super(message);
+	}
+
+	public DarException() {
+		super();
+	}
+
+	public DarException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DarException(Throwable cause) {
+		super(cause);
+	}
+	
+	
+	
+	
+
+}
--- src/routage/Dispatcher.java
+++ src/routage/Dispatcher.java
@@ -1,15 +1,11 @@
 package routage;
 
-import java.awt.Point;
-import java.awt.image.RescaleOp;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-import java.util.List;
-
+import exception.DarException;
 import serveur.ReponseHttp;
 import serveur.RequeteHttp;
 import serveur.URLHttp;
 import tools.Fichier;
+import tools.Reflexion;
 
 public class Dispatcher {
 	private RequeteHttp requete;
@@ -22,32 +18,15 @@ public class Dispatcher {
 	public ReponseHttp process() {
 		String content = Fichier
 				.lectureFichier("C:\\Users\\khelifa.berrefas\\DAR\\workspace\\DAR1\\src\\routage\\routage.txt");
+		
 		String classe = "application.WebJouet";
 		String methode = "getListPoint";
 
 		ReponseHttp reponse = new ReponseHttp();
 
 		try {
-			Class c = Class.forName(classe); // Accès à la classe Livre
-
-			System.out.println(c.getConstructors());
-
-			Constructor constr = c.getConstructor(); // Obtenir le
-														// constructeur
-														// (RequeteHttp)
-			Object o = constr.newInstance(); // -> new Livre("Programmation
-												// Java", 120);
-
-			Method method = c.getMethod(methode); // Obtenir la méthode
-													// getNombreDeFeuilles(int)
-			List<Point> pts = (List<Point>) method.invoke(o); // ->
-																// o.getNombreDeFeuilles(2);
-			System.out.println("#########################");
-			System.out.println(pts);
-			reponse.setEntete("Content-type", "text/html");
-			reponse.setCorps(pts.toString());
-			System.out.println("#########################");
-		} catch (Exception e) {
+			Reflexion.invokeMethod(classe, methode);
+		} catch (DarException e) {
 			e.printStackTrace(System.out);
 			reponse.setStatut(400);
 			reponse.setCorps(e.getMessage());
