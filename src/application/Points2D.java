f2cfe2f2871704cdf67189794806deac6ae8dd2d
khelifaB <khelifa.berrefas@hotmail.fr>
2017-02-27 13:26:52 +0100

    Service Application Point2D
    Signed-off-by: khelifaB <khelifa.berrefas@hotmail.fr>
    
    rebase

--- src/application/Points2D.java
+++ src/application/Points2D.java
@@ -1,12 +1,124 @@
 package application;
 
 import java.awt.Point;
+import java.util.Arrays;
 import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+
+import serveur.Dispatcher;
+import serveur.ReponseHttp;
+import serveur.RequeteHttp;
+import serveur.Service;
 
 public class Points2D extends Service{
 
-	private static HashMap<Integer, Point> points = new HashMap<Integer, Point>();
-	
-	
-	
+	private static HashMap<String, Point> points = new HashMap<String, Point>();
+
+	public Points2D() {
+
+	}
+
+	public ReponseHttp list() {
+		ReponseHttp reponse = new ReponseHttp();
+		StringBuilder sb =new StringBuilder();
+		JSONObject jo = new JSONObject();
+		for(String id : points.keySet()){
+			JSONObject jp = new JSONObject();
+			jp.put("x", points.get(id).x);
+			jp.put("y", points.get(id).y);
+			jo.put(id, jp);
+		}
+		reponse.setCorps(jo.toString());
+		return reponse;
+	}
+
+	public ReponseHttp getXbyId(String id){
+		ReponseHttp reponse = new ReponseHttp();
+		Point p = points.get(id);
+		if(p==null){
+			reponse.setCorps("id:"+ id +" de point inconnu");
+			return reponse;
+		}
+
+		reponse.setCorps(p.x+"");
+		return reponse;
+	}
+
+	public ReponseHttp getYbyId(String id){
+		ReponseHttp reponse = new ReponseHttp();
+		Point p = points.get(id);
+		if(p==null){
+			reponse.setCorps("id:"+ id +" de point inconnu");
+			return reponse;
+		}
+
+		reponse.setCorps(p.y+"");
+		return reponse;
+	}
+
+	public ReponseHttp putPointById(String id){
+		ReponseHttp reponse = new ReponseHttp();
+		Point p = points.get(id);
+		if(p==null){
+			reponse.setCorps("id:"+ id +" de point inconnu");
+			return reponse;
+		}
+
+		int x,y;
+		if(!"".equals(requete.getParametre("x")) && requete.getParametre("x")!=null){
+			x = Integer.valueOf(requete.getParametre("x"));
+			p.x=x;
+			reponse.setCorps("point id:"+id+" modification de x");
+		}
+		
+		if(!"".equals(requete.getParametre("y")) && requete.getParametre("y")!=null ){
+			y = Integer.valueOf(requete.getParametre("y"));
+			p.y=y;
+			reponse.setCorps(reponse.getCorps()+" point id:"+id+" modification de y");
+		}
+		
+
+		points.put(id, p);
+		return reponse;
+	}
+
+	public ReponseHttp creerPoint(){
+		ReponseHttp reponse = new ReponseHttp();
+		try{
+			String[] parametre = requete.getCorps().split("&");
+			
+			HashMap<String, String> parametres= new HashMap<String, String>();
+
+			for(int i=0;i<parametre.length; i++){
+				System.out.println("parametre : "+Arrays.toString(parametre));
+				parametres.put(parametre[i].split("=")[0], parametre[i].split("=")[1]);
+			}
+			
+			String x = parametres.get("x");
+			String y = parametres.get("y");
+			
+			Point p = new Point(Integer.parseInt(x), Integer.parseInt(y));
+			int id = (points.size()+1);
+			points.put(id+"", p);
+			
+			reponse.setCorps(id+"");
+
+			return reponse;
+		}catch(Exception e){
+			e.printStackTrace();
+			reponse.setStatut(400);
+			reponse.setCorps(e.getMessage());
+			return reponse;
+		}
+	}
+
+	public ReponseHttp supprimerPoint(String id){
+		ReponseHttp reponse = new ReponseHttp();
+		if(points.remove(id)!=null)
+			reponse.setCorps("ok");
+		else
+			reponse.setCorps("ko");
+		return reponse;
+	}
 }
---
+++ src/serveur/Dispatcher.java
@@ -0,0 +1,122 @@
+package serveur;
+
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import tools.Reflexion;
+
+public class Dispatcher {
+	private RequeteHttp requete;
+	private URLHttp url;
+
+	public Dispatcher(RequeteHttp requete) {
+		this.requete = requete;
+	}
+
+	//	public ReponseHttp process() {
+	//		String content = Fichier.lectureFichier("C:\\Users\\khelifa.berrefas\\DAR\\workspace\\DAR1\\src\\routage\\routage.txt");
+	//		String classe = "application.WebJouet";
+	//		String methode = "getListPoint";
+	//
+	//		ReponseHttp reponse = new ReponseHttp();
+	//
+	//		try {
+	//			Class c = Class.forName(classe); // Accès à la classe Livre
+	//
+	//			System.out.println(c.getConstructors());
+	//
+	//			Constructor constr = c.getConstructor(); // Obtenir le
+	//			// constructeur
+	//			// (RequeteHttp)
+	//			Object o = constr.newInstance(); // -> new Livre("Programmation
+	//			// Java", 120);
+	//
+	//			Method method = c.getMethod(methode); // Obtenir la méthode
+	//			// getNombreDeFeuilles(int)
+	//			List<Point> pts = (List<Point>) method.invoke(o); // ->
+	//			// o.getNombreDeFeuilles(2);
+	//			System.out.println("#########################");
+	//			System.out.println(pts);
+	//			reponse.setEntete("Content-type", "text/html");
+	//			reponse.setCorps(pts.toString());
+	//			System.out.println("#########################");
+	//			Reflexion.invokeMethod(classe, methode);
+	//		} catch (Exception e) {
+	//		}
+	//
+	//		return reponse;
+	//	}
+
+	public static ReponseHttp getService(RequeteHttp requete) {
+
+		// url /nom application/...
+
+		String[] paramTab = requete.getUrl().getChemin().split("/");
+		String nomApplication = paramTab[1];
+
+		JSONParser parser = new JSONParser();
+
+		try {     
+			JSONObject jsonObject =  (JSONObject)  parser.parse(new FileReader("mappingJSON/"+nomApplication+".json"));
+
+			String url = (String) jsonObject.get("url");
+			System.out.println(url);
+
+			String classe = (String) jsonObject.get("classe");
+			
+			JSONArray methodes = (JSONArray)jsonObject.get("methodes");
+
+			for(Object o : methodes){
+				JSONObject oj = (JSONObject) o;
+				String urli = (String) oj.get("url");
+				String verbe = (String) oj.get("type");
+				String methodeJava = (String)oj.get("methode");
+
+				if(verbe.equals(requete.getVerbe().toString())) {
+					String chemin = requete.getUrl().getChemin().substring(requete.getUrl().getChemin().indexOf('/', 1));
+					
+					if(chemin.matches(urli)){
+
+						Pattern p = Pattern.compile(urli);
+						Matcher m = p.matcher(chemin);
+						String[] paramatere=null;
+						
+						if(m.matches()){
+							paramatere = new String[m.groupCount()];
+							for(int i=1; i<= m.groupCount(); i++){
+								paramatere[i-1]=m.group(i);
+								System.out.println(paramatere[i-1]);
+							}
+						}
+						System.out.println(Arrays.toString(paramatere));
+						ReponseHttp reponse =(ReponseHttp) Reflexion.invokeMethod(classe, methodeJava, paramatere, requete);
+						return reponse;
+
+					}
+
+				}
+
+			}
+
+
+		} catch (Exception e) {
+			ReponseHttp reponse = new ReponseHttp();
+			reponse.setStatut(404);
+			reponse.setCorps(e.getMessage());
+			e.printStackTrace();
+			return reponse;
+		}
+
+		ReponseHttp reponse = new ReponseHttp();
+		reponse.setStatut(404);
+		reponse.setCorps("url non trouvee");
+		return reponse;
+	}
+
+}
